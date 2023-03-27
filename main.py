f26605f66ce76adb75cf10b35b8563dc379dd5e0
NoWay_y <74825224+NoWauu@users.noreply.github.com>
2023-03-27 10:10:34 +0200

    aa

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 import pygame
-from modules.pieces import WhitePieces, BlackPieces
 from modules.plateau import Plateau
+from modules.pieces import Pieces
 
 
 pygame.init()
 
 WINDOWSIZE = (830, 830)
 
-WHITECASE = (200, 173, 127)
-BLACKCASE = (91, 60, 17)
+WHITECASE_COLOR = (200, 173, 127)
+BLACKCASE_COLOR = (91, 60, 17)
 
 FPS = 60
 
@@ -16,17 +16,13 @@ SCREEN = pygame.display.set_mode(WINDOWSIZE)
 pygame.display.set_caption("Chess")
 
 # dessine le plateau
-plateau = Plateau(SCREEN, BLACKCASE, WHITECASE)
+plateau = Plateau(SCREEN, BLACKCASE_COLOR, WHITECASE_COLOR)
 plateau.draw_background()
 plateau.draw_lines()
 
-# place les pièces blanches
-pieces_blanches = WhitePieces()
-pieces_blanches.draw(SCREEN)
-
-# place les pièces noires
-pieces_noires = BlackPieces()
-pieces_noires.draw(SCREEN)
+# dessine les pieces de base sur le plateau
+pieces = Pieces()
+pieces.place(SCREEN)
 
 # True : blanc / False : noir
 turn = True
@@ -39,8 +35,7 @@ while PLAYING:
         if event.type == pygame.QUIT:
             PLAYING = False
         if event.type == pygame.MOUSEBUTTONUP:
-            pieces_blanches.click()
-
+            pass
 
     # actualise l'écran
     pygame.display.flip()
--- modules/pieces.py
+++ modules/pieces.py
@@ -1,116 +1,31 @@
 import pygame
 
-class WhitePieces:
-    """Pieces blanches"""
+class Pieces:
     def __init__(self) -> None:
-        self.is_check = False
-        self.is_checkmate = False
-        self.is_promotion = False
-        self.is_turn = True
-
         self.white_pieces = {
-            "king": pygame.image.load("IMG/white/king.png"),
-            "queen": pygame.image.load("IMG/white/queen.png"),
-            "rook": pygame.image.load("IMG/white/rook.png"),
-            "bishop": pygame.image.load("IMG/white/bishop.png"),
-            "knight": pygame.image.load("IMG/white/knight.png"),
-            "pon": pygame.image.load("IMG/white/pon.png"),
+            "rook1": [pygame.image.load("IMG/white/rook.png"), [30, 730], (100, 100)],
+            "knight1": [pygame.image.load("IMG/white/knight.png"), [130, 730], (100, 100)],
+            "bishop_black": [pygame.image.load("IMG/white/bishop.png"), [230, 730], (100, 100)],
+            "queen": [pygame.image.load("IMG/white/queen.png"), [330, 730], (100, 100)],
+            "king": [pygame.image.load("IMG/white/king.png"), [430, 730], (100, 100)],
+            "bishop_white": [pygame.image.load("IMG/white/bishop.png"), [530, 730], (100, 100)],
+            "knight2": [pygame.image.load("IMG/white/knight.png"), [630, 730], (100, 100)],
+            "rook2": [pygame.image.load("IMG/white/rook.png"), [730, 730], (100, 100)]
         }
 
-        self.white_positions = {
-            "king": (430, 730),
-            "queen": (330, 730),
-            "rook1": (30, 730),
-            "rook2": (730, 730),
-            "white_bishop": (230, 730),
-            "black_bishop": (530, 730),
-            "knight1": (130, 730),
-            "knight2": (630, 730),
-            "pon1": (30, 630),
-            "pon2": (130, 630),
-            "pon3": (230, 630),
-            "pon4": (330, 630),
-            "pon5": (430, 630),
-            "pon6": (530, 630),
-            "pon7": (630, 630),
-            "pon8": (730, 630)
+        self.black_pieces = {
+            "rook1": [pygame.image.load("IMG/black/rook.png"), [30, 730], (100, 100)],
+            "knight1": [pygame.image.load("IMG/black/knight.png"), [130, 730], (100, 100)],
+            "bishop_black": [pygame.image.load("IMG/black/bishop.png"), [230, 730], (100, 100)],
+            "queen": [pygame.image.load("IMG/black/queen.png"), [330, 730], (100, 100)],
+            "king": [pygame.image.load("IMG/black/king.png"), [430, 730], (100, 100)],
+            "bishop_white": [pygame.image.load("IMG/black/bishop.png"), [530, 730], (100, 100)],
+            "knight2": [pygame.image.load("IMG/black/knight.png"), [630, 730], (100, 100)],
+            "rook2": [pygame.image.load("IMG/black/rook.png"), [730, 730], (100, 100)]
         }
 
-        self.colliders = {
-            "king": self.white_pieces["king"].get_rect().move(self.white_positions["king"]),
-            "queen": self.white_pieces["queen"].get_rect().move(self.white_positions["queen"]),
-            "rook1": self.white_pieces["rook"].get_rect().move(self.white_positions["rook1"]),
-            "rook2": self.white_pieces["rook"].get_rect().move(self.white_positions["rook2"]),
-            "bishop1": self.white_pieces["bishop"].get_rect().move(self.white_positions["white_bishop"]),
-            "bishop2": self.white_pieces["bishop"].get_rect().move(self.white_positions["black_bishop"]),
-            "knight1": self.white_pieces["knight"].get_rect().move(self.white_positions["knight1"]),
-            "knight2": self.white_pieces["knight"].get_rect().move(self.white_positions["knight2"]),
-            "pon1": self.white_pieces["pon"].get_rect().move(self.white_positions["pon1"]),
-            "pon2": self.white_pieces["pon"].get_rect().move(self.white_positions["pon2"]),
-            "pon3": self.white_pieces["pon"].get_rect().move(self.white_positions["pon3"]),
-            "pon4": self.white_pieces["pon"].get_rect().move(self.white_positions["pon4"]),
-            "pon5": self.white_pieces["pon"].get_rect().move(self.white_positions["pon5"]),
-            "pon6": self.white_pieces["pon"].get_rect().move(self.white_positions["pon6"]),
-            "pon7": self.white_pieces["pon"].get_rect().move(self.white_positions["pon7"]),
-            "pon8": self.white_pieces["pon"].get_rect().move(self.white_positions["pon8"])
-        }
-
-    def draw(self, screen):
-        """place les pièces sur l'échiquier"""
-        screen.blit(self.white_pieces["king"], (430, 730))
-        screen.blit(self.white_pieces["queen"], (330, 730))
-
-        screen.blit(self.white_pieces["rook"], (30, 730))
-        screen.blit(self.white_pieces["rook"], (730, 730))
-
-        screen.blit(self.white_pieces["bishop"], (230, 730))
-        screen.blit(self.white_pieces["bishop"], (530, 730))
-
-        screen.blit(self.white_pieces["knight"], (130, 730))
-        screen.blit(self.white_pieces["knight"], (630, 730))
-
-        for i in range(30, 800, 100):
-            screen.blit(self.white_pieces["pon"], (i, 630))
-
-    def click(self):
-        """montre les possibilités de déplacement en fonction de la pièce cliquée"""
-        # récupère le clic de la souris
-        mouse_pos = pygame.mouse.get_pos()
-        for piece in self.colliders.values():
-            if piece.collidepoint(mouse_pos):
-                print(piece)
-
-class BlackPieces:
-    """Gestion des pièces noires"""
-    def __init__(self) -> None:
-        self.is_check = False
-        self.is_checkmate = False
-        self.is_promotion = False
-        self.is_turn = False
-
-
-        self.pieces = {
-            "king": pygame.image.load("IMG/black/king.png"),
-            "queen": pygame.image.load("IMG/black/queen.png"),
-            "rook": pygame.image.load("IMG/black/rook.png"),
-            "bishop": pygame.image.load("IMG/black/bishop.png"),
-            "knight": pygame.image.load("IMG/black/knight.png"),
-            "pon": pygame.image.load("IMG/black/pon.png"),
-        }
-
-    def draw(self, screen):
-        """mets les pieces noires sur l'échiquier"""
-        screen.blit(self.pieces["king"], (430, 30))
-        screen.blit(self.pieces["queen"], (330, 30))
-
-        screen.blit(self.pieces["rook"], (30, 30))
-        screen.blit(self.pieces["rook"], (730, 30))
-
-        screen.blit(self.pieces["bishop"], (230, 30))
-        screen.blit(self.pieces["bishop"], (530, 30))
-
-        screen.blit(self.pieces["knight"], (130, 30))
-        screen.blit(self.pieces["knight"], (630, 30))
-
-        for i in range(30, 800, 100):
-            screen.blit(self.pieces["pon"], (i, 130))
+    def place(self, screen: pygame.Surface):
+        for piece in self.white_pieces.values():
+            screen.blit(piece[0], piece[1])
+        for piece in self.black_pieces.values():
+            screen.blit(piece[0], piece[1])
